6f02b9dc23506365a0df33a7690eb2a9b7f0aaee
Alyssa <85774363+alyssaayx@users.noreply.github.com>
2022-09-02 11:21:17 +0800

    Base

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-	
+    pins.servoWritePin(AnalogPin.P1, 100)
 })
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P1, 50)
+})
+let angle = 0
+let millig = 0
+basic.pause(500)
+pins.servoWritePin(AnalogPin.P1, 90)
 basic.forever(function () {
-	
+    millig = input.acceleration(Dimension.X)
+    angle = pins.map(
+    millig,
+    -1023,
+    1023,
+    0,
+    180
+    )
+    pins.servoWritePin(AnalogPin.P0, angle)
 })
